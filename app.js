2468a554085ea4581cc56c216b60bbfe95100599
Sabina Catea <cateasabina5@gmail.com>
2024-01-12 09:28:05 +0200

    Mongo Atlas updates

--- app.js
+++ app.js
@@ -25,12 +25,30 @@ const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToxen = process.env.MAPBOX_TOXEN;
 const geocoder = mbxGeocoding({accessToken: mapBoxToxen});
 const { campgroundSchema, reviewSchema } = require('./schemas');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const { MongoStore } = require('connect-mongo');
+// const dbURL = process.env.DB_URL;
+const dbURL = 'mongodb://0.0.0.0:27017/yelp-camp'
+const MongoDBStore = require("connect-mongo")(session);
 
-mongoose.connect('mongodb://0.0.0.0:27017/yelp-camp');
+// mongodb://0.0.0.0:27017/yelp-camp
+mongoose.connect(dbURL);
 
+const store = new MongoDBStore({
+    url: dbURL,
+    secret: 'thisshouldbesecret',
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function(e){
+    console.log("SESSION STORE ERROR", e)
+})
 
 const sessionConfig = {
-    secret:' secretkey',
+    store,
+    name: 'session',
+    secret:'thisshouldbesecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -51,6 +69,55 @@ db.once("open",()=>{
 const app = express();
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({contentSecurityPolicy: false}));
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+//This is the array that needs added to
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dsy4kpcfr/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
+app.use(mongoSanitize());
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -62,7 +129,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next)=>{
     res.locals.currentUser = req.user;
     console.log('currentUser', res.locals.currentUser)
-    console.log('req user', req.user)
+    console.log('req user', req.query)
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
